4e35b29c165e959cdff33fe6543f919c929ecb7c
AnasNaamneh <naamnih.anas@gmail.com>
2021-12-09 16:40:11 +0200

    adding buttons to todo list

--- project2.js
+++ project2.js
@@ -1,1 +1,66 @@
-console.log("Project 2");
+
+
+let toDoList = [
+    
+    {task: "Wake Up",id:1},
+    {task: "Eat",id:2},
+    {task: "Code",id:3},
+    {task: "Sleep",id:4},
+] 
+
+
+
+
+
+const body = $("body");
+
+const unorderedList = $("<ul></ul>");
+unorderedList.appendTo(body);
+
+const input = $("<input>");
+input.appendTo(body);
+
+const button = $("<button></button>")
+button.text("add");
+button.appendTo(body);
+
+
+const toDo = () => {
+toDoList.forEach((element,i)=>{
+let listItem = $("<li></li>")
+const newbutton = $("<button>Delete</button>")
+const updatebutton = $("<button>Update</button>")
+
+
+newbutton.on("click",()=>{
+
+    listItem.remove();
+})
+
+updatebutton.on("click",()=>{
+
+listItem.text(input.vl());
+
+
+})
+
+
+
+listItem.text(element.task);
+listItem.appendTo(unorderedList);
+newbutton.appendTo(listItem);
+updatebutton.appendTo(listItem);
+
+})
+
+}
+toDo();
+
+const addtoList = () => {
+
+toDoList = [];
+toDoList.push( { task: input.val()});
+toDo()
+}
+
+button.on("click" , addtoList);
