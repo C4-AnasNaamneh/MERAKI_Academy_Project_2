4a67755980c6397e063da72dd39b6454c88a9456
AnasNaamneh <naamnih.anas@gmail.com>
2021-12-10 19:32:42 +0200

    fixing value duplicating

--- project2.js
+++ project2.js
@@ -1,173 +1,137 @@
-let toDoList = []; 
-  const body = $("body");
-  
-  const container = $("#container");
-  
-const title = $("<div></div>")
+let toDoList = [];
+const body = $("body");
+
+const container = $("#container");
+
+const title = $("<div></div>");
 
 title.addClass("title");
-title.appendTo(container)
+title.appendTo(container);
 
-  const header = $("<h1>Schedule your day</h1>");
+const header = $("<h1>Schedule your day</h1>");
 
-  header.addClass("h1")
-  header.appendTo(title)
+header.addClass("h1");
+header.appendTo(title);
 
-  const tasks = $("<div></div>");
-  tasks.appendTo(container);
-  
-  
-  const completed = $("<div></div>");
-  completed.appendTo(container);
-  
-  const anchor = $("<a href=#></a>");
-  anchor.text("Tasks")
-  anchor.appendTo(tasks)
-  
-  const anchor1 = $("<a href =# ></a>");
-  anchor1.text("Completed")
-  anchor1.appendTo(completed);
-  
-  
-  tasks.addClass("tasks");
-  completed.addClass("completed");
-  
-  anchor.addClass("anchor");
-  anchor1.addClass("anchor");
-  
-  const divContainer = $("<div></div>");
-  divContainer.addClass("divContainer");
-  
+const tasks = $("<div></div>");
+tasks.appendTo(container);
 
-  const inputbuttondiv = $("<div></div>");
-  inputbuttondiv.addClass("inputbuttondiv");
-  inputbuttondiv.appendTo(body)
+const completed = $("<div></div>");
+completed.appendTo(container);
 
-  
-  divContainer.appendTo(body);
-  
-  
-  const unorderedList = $("<ul></ul>");
-  unorderedList.addClass("unorderedList");
-  unorderedList.appendTo(divContainer);
-  
-  
-  const input = $(`<input placeholder="Add New Task...">`);
-  input.appendTo(inputbuttondiv);
-  
-  const inputDate = $("<input  type = date></input");
-  inputDate.addClass("inputDate");
-  inputDate.appendTo(inputbuttondiv);
-  
-  const addbutton = $("<button></button>");
-  addbutton.text("add");
-  addbutton.addClass("addbutton")
-  addbutton.appendTo(inputbuttondiv);
-  
+const anchor = $("<a href=#></a>");
+anchor.text("Tasks");
+anchor.appendTo(tasks);
 
-  
-  const toDo = () => {
+const anchor1 = $("<a href =# ></a>");
+anchor1.text("Completed");
+anchor1.appendTo(completed);
 
-    
-  toDoList.forEach((element,i)=>{
-  
-  
-  let listItem = $("<li></li>");
-  listItem.addClass("listItem");
-  const deletebutton = $("<button>Delete</button>");
-  const updatebutton = $("<button>Update</button>");
-  const completedbutton = $("<button>Completed</button>");
-  //const inputDate = $("<input type = date></input");
-  
-  deletebutton.addClass("deletebutton");
-  updatebutton.addClass("updatebutton");
-  completedbutton.addClass("completedbutton");
+tasks.addClass("tasks");
+completed.addClass("completed");
 
-  deletebutton.on("click",()=>{
-  
+anchor.addClass("anchor");
+anchor1.addClass("anchor");
+
+const divContainer = $("<div></div>");
+divContainer.addClass("divContainer");
+
+const inputbuttondiv = $("<div></div>");
+inputbuttondiv.addClass("inputbuttondiv");
+inputbuttondiv.appendTo(body);
+
+divContainer.appendTo(body);
+
+const unorderedList = $("<ul></ul>");
+unorderedList.addClass("unorderedList");
+unorderedList.appendTo(divContainer);
+
+const input = $(`<input placeholder="Add New Task...">`);
+input.appendTo(inputbuttondiv);
+
+
+const inputDate = $("<input  type = date></input");
+inputDate.addClass("inputDate");
+inputDate.appendTo(inputbuttondiv);
+
+const addbutton = $("<button></button>");
+addbutton.text("add");
+addbutton.addClass("addbutton");
+addbutton.appendTo(inputbuttondiv);
+
+const toDo = () => {
+  unorderedList.text("");
+
+  toDoList.forEach((element, i) => {
+    let listItem = $("<li></li>");
+    listItem.addClass("listItem");
+    const deletebutton = $("<button>Delete</button>");
+    const updatebutton = $("<button>Update</button>");
+    const completedbutton = $("<button>Completed</button>");
+    //const inputDate = $("<input type = date></input");
+    listItem.text(element.task);
+    listItem.appendTo(unorderedList);
+    // inputDate.appendTo(inputbuttondiv);
+    // inputDate.appendTo(listItem);
+    deletebutton.appendTo(listItem);
+    updatebutton.appendTo(listItem);
+    completedbutton.appendTo(listItem);
+
+    deletebutton.addClass("deletebutton");
+    updatebutton.addClass("updatebutton");
+    completedbutton.addClass("completedbutton");
+
+    deletebutton.on("click", () => {
       listItem.remove();
-  })
-  
-  
-  updatebutton.on("click",()=>{  
-  
-  listItem.text(input.val());
-  deletebutton.appendTo(listItem);
-  updatebutton.appendTo(listItem);
-  completedbutton.appendTo(listItem);
-  
+    });
 
-
-  deletebutton.on("click",()=>{
-  
-      listItem.remove();
-  })
-  
-  updatebutton.on("click" ,()=>{
-  
+    updatebutton.on("click", () => {
       listItem.text(input.val());
-        
-  
-  })
-       //hide pages and move to completed page or move to array completed
-      // we can make if (completed button);  
-  })
+      deletebutton.appendTo(listItem);
+      updatebutton.appendTo(listItem);
+      completedbutton.appendTo(listItem);
 
-  tasks.on("click" ,()=>{
+      deletebutton.on("click", () => {
+        listItem.remove();
+      });
 
-  inputbuttondiv.hide();
+      updatebutton.on("click", () => {
+        listItem.text(input.val());
+      });
+      //hide pages and move to completed page or move to array completed
+      // we can make if (completed button);
+    });
 
-  })
+    tasks.on("click", () => {
+      inputbuttondiv.hide();
+    });
 
-completedbutton.on("click",()=>{
+    completedbutton.on("click", () => {
+      //listItem.text(input.val())
+      listItem.text(input.val());
+    });
 
+    completed.on("click", () => {
+      if (completed) {
+      }
+    });
 
-  listItem.text(input.val())
-
-})
-
-
-completed.on("click",()=>{
-
-
-})
-
-
-  inputDate.on("change",()=>{
-  
+    inputDate.on("change", () => {
       listItem.text(inputDate.val());
-  })
-  
-  
-  
-  
-  
-  listItem.text(element.task );
-  listItem.appendTo(unorderedList);
-  inputDate.appendTo(divContainer);
-  inputDate.appendTo(listItem);
-  deletebutton.appendTo(listItem);
-  updatebutton.appendTo(listItem);
-  completedbutton.appendTo(listItem);
-  
-  
-  })
-  
-  }
-  toDo();
-  
-  const addtoList = () => {
-    
-  
+    });
+  });
+};
+toDo();
+
+const addtoList = () => {
   //toDoList = [];
-  toDoList.push( { task: input.val()});
-  toDo()
-  }
-  
-  addbutton.on("click" , addtoList);
+  toDoList.push({ task: input.val() });
+  toDo();
+};
 
+addbutton.on("click", addtoList);
 
-  /*let completedList = [
+/*let completedList = [
     
     {task: input.val(),id: "completed"},
     {task: input.val(),id: "completed"},
